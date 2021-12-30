71af5bda43a5c0a336b761f94ff34f9216bd2706
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2021-12-30 12:52:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,5 +8,38 @@ function modelReady() {
 }
 
 function gotResults(error, results) {
-    console.log("Model Loaded");
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - ' + (results[0].confidence * 100).toFixed(2) + "%";
+        img1 = document.getElementById("alien1");
+        img2 = document.getElementById("alien2");
+        img3 = document.getElementById("alien3");
+        img4 = document.getElementById("alien4");
+        if (results[0].label == "Clap") {
+            img1.src = "aliens-01.gif";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.png";
+        } else if (results[0].label == "Snap") {
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.gif";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.png";
+        } else if (results[0].label == "Bell") {
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.gif";
+            img4.src = "aliens-04.png";
+        } else {
+            img1.src = "aliens-01.png";
+            img2.src = "aliens-02.png";
+            img3.src = "aliens-03.png";
+            img4.src = "aliens-04.gif";
+        }
+
+    }
+
 }
